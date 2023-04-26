06b01016fd1ae7a5430afe5a26c8576c0caba40d
Ege İliklier <ege.iliklier@gmail.com>
2023-04-26 17:14:14 +0300

    refactor: resolveSideEffect does not interfere with path resolution

--- src/plugins/resolveSideEffects.ts
+++ src/plugins/resolveSideEffects.ts
@@ -1,18 +1,24 @@
 import type { Plugin as VitePlugin } from 'vite'
 
-const QUASAR_PURE_RE = /([\/\\])quasar\1src\1(components|composables|directives)\1/
+const QUASAR_PURE_RE = /([\\\/])node_modules\1quasar\1src\1(components|composables|directives)\1/
 
 export const resolveQuasarModuleSideEffectsPlugin = (): VitePlugin => {
   return {
     name: 'quasar:resolve-side-effects',
     enforce: 'pre',
-    resolveId(id) {
-      if (id.includes('node_modules') && QUASAR_PURE_RE.test(id)) {
-        return {
-          id,
-          moduleSideEffects: false,
-        }
+    async resolveId(source, importer, options) {
+      if (!QUASAR_PURE_RE.test(source))
+        return
+
+      const resolution = await this.resolve(source, importer, {
+        ...options,
+        skipSelf: true,
+      })
+
+      if (resolution) {
+        resolution.moduleSideEffects = false
       }
+      return resolution
     },
   }
 }
