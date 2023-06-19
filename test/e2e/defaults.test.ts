f6bbae04fa0d929338c5c04edb57ff07b0d77040
Ege İliklier <ege.iliklier@gmail.com>
2023-06-19 17:38:19 +0300

    chore: switched to jsdom while testing

--- test/e2e/defaults.test.ts
+++ test/e2e/defaults.test.ts
@@ -1,7 +1,8 @@
+// @vitest-environment node
 import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
-import { $fetch, setup, useTestContext } from '@nuxt/test-utils'
-import { Window } from 'happy-dom'
+import { $fetch, setup } from '@nuxt/test-utils'
+import { JSDOM } from 'jsdom'
 
 describe('ssr', async () => {
   await setup({
@@ -11,30 +12,25 @@ describe('ssr', async () => {
   it('renders button with defaults', async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch('/')
-    const ctx = useTestContext()
-    const { document } = new Window({
-      url: `${ctx.url}/`,
-    })
-    document.write(html)
+    const dom = new JSDOM(html)
+    const { document } = dom.window
     const btn = document.querySelector('#btn_default')
     expect(btn).toBeTruthy()
-    expect([...btn.classList.values()]).toContain('bg-primary')
-    expect(btn.textContent).toBe('Default')
+    expect([...btn!.classList.values()]).toContain('bg-primary')
+    expect(btn!.textContent).toBe('Default')
   })
 
   it('renders button that overrides the defaults', async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch('/')
-    const ctx = useTestContext()
-    const { document } = new Window({
-      url: `${ctx.url}/`,
-    })
+    const dom = new JSDOM(html, {})
+    const { document } = dom.window
     document.write(html)
     const btn = document.querySelector('#btn_warning')
-    const classes = [...btn.classList.values()]
+    const classes = [...btn!.classList.values()]
     expect(btn).toBeTruthy()
     expect(classes).toContain('bg-warning')
     expect(classes).not.toContain('bg-primary')
-    expect(btn.textContent).toBe('Warning')
+    expect(btn!.textContent).toBe('Warning')
   })
 })
