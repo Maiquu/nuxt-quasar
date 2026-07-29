38aaa823bc72678167d2bfb269bd5d894a237d40
Ege İliklier <ege.iliklier@gmail.com>
2026-07-29 23:38:05 +0300

    style: test fixture lint

--- test/fixtures/extras-v2/app.vue
+++ test/fixtures/extras-v2/app.vue
@@ -1,11 +1,12 @@
-
 <script setup>
 const icon = typeof biApp !== 'undefined' ? biApp : ''
 </script>
 
-
 <template>
   <div>
-    <QIcon v-if="icon" :name="icon" />
+    <QIcon
+      v-if="icon"
+      :name="icon"
+    />
   </div>
 </template>
